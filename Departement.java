35600d9322dea11b3c402bdd8fca8f35a2c9c1d6
rafeu <rafeu@users.noreply.github.com>
2016-04-12 15:52:26 -0400

    Finalisation pour remise
    Ajout de création vendeur dans le menu
    Correction exception pour tauxHeure et nbrHeure
    ajout de "Vendeur" dans les toString et afficher() de vendeur
    Amélioration menu

--- Departement.java
+++ Departement.java
@@ -1,98 +1,106 @@
-/**	B - Dans le fichier  Departement.java, definir la classe Departement
- * 
- * 	1 - Les attributs (choisir les types appropries): 
- * 		- nom departement 
- * 		- nombre d'employes 
- * 		- tableau d'employes (20  au  max) 
- * 			Definir  un  main() et faire des tests au fur et à mesure,
- *  		dans lequel vous creerez un  objet de cette classe 
- *  		et affichez ses informations, avec les donnees de votre choix. 
- * 	2 - Les methodes 
- * 		- masseSalariale() : 
- * 			retourne  le  total  des salaires  bruts  de  tous  les  employes
- * 			figurant dans le tableau d'employes 
- * 		- moyenne Salariale() :  
- * 			retourne  la  moyenne  des  salaires  bruts
- *   		de  tous  les  employes figurant dans le tableau d'employes 
- * 
- * @author Raphael Duchaene 19/02/2016
- * 
- * 
+/**
+ * B - Dans le fichier Departement.java, definir la classe Departement
+ *
+ * 1 - Les attributs (choisir les types appropries): - nom departement - nombre
+ * d'employes - tableau d'employes (20 au max) Definir un main() et faire des
+ * tests au fur et à mesure, dans lequel vous creerez un objet de cette classe
+ * et affichez ses informations, avec les donnees de votre choix. 2 - Les
+ * methodes - masseSalariale() : retourne le total des salaires bruts de tous
+ * les employes figurant dans le tableau d'employes - moyenne Salariale() :
+ * retourne la moyenne des salaires bruts de tous les employes figurant dans le
+ * tableau d'employes
+ *
+ * @author Raphael Duchaine 09/04/2016
+ *
+ *
  */
-
 public class Departement {
-  //Attributs
-	final int MAX=20;
-	private String nomDepartement;
-	private int nbrEmploye;
-	private Employe[] tabEmploye=new Employe[MAX];
-  
-	public Employe getTabEmploye(int i) {
-		return tabEmploye[i];
-	}
-	public void setTabEmploye(int i,Employe employe) {
-		this.tabEmploye[i] = employe;
-	}
-	
-	public int getNbrEmploye() {
-		return nbrEmploye;
-	}
-	public void setNbrEmploye(int nbrEmploye) {
-		this.nbrEmploye = nbrEmploye;
-	}
-	
-	public String getNomDepartement() {
-		return nomDepartement;
-	}
-	public void setNomDepartement(String nomDepartement) {
-		this.nomDepartement = nomDepartement;
-	}
-	
-	//Constructeurs	
-	public Departement(){
-		this.setNomDepartement("Departement par defaut");
-	}
-	//Constructeur demandant le nom du departement
-	public Departement(String nomDepartement){
-		this.setNomDepartement(nomDepartement);
-	}
-	//toString
-	public String toString(){
-		try{
-			return getNomDepartement()+": "+getNbrEmploye()+" Employes \n Masse Salariale: "+masseSalariale()+"\n Moyenne Salariale: "+moyenneSalariale();
-		}catch(Exception e){
-			throw new ArithmeticException(e.toString());
-		}
-	}
-	
-	//Autres Methodes
-	//Calcul de la masse salariale
-	public double masseSalariale(){
-		double masse=0;
-		for(int i=0;i<getNbrEmploye();i++)
-			masse+=getTabEmploye(i).salaireBrut();
-		
-		return masse;	
-	}//Calcul de la moyenne salariale 
-	public double moyenneSalariale() throws Exception{
-		try{
-			return masseSalariale()/getNbrEmploye();
-		}catch(Exception e){
-			throw new ArithmeticException("Il n'y a pas d'employés!!");
-		}
-	}
-	//Methode permettant d'ajouter un employe au bout de tabEmploye
-	public void addEmploye(String nom,String prenom,String date,double salaireEmploye,int heures){
-		setTabEmploye(getNbrEmploye(),new Employe(nom,prenom,date,heures,salaireEmploye));
-		setNbrEmploye(getNbrEmploye() + 1);
-	}
-	//Main pour tester la classe
+
+    //Attributs
+    private final int MAX = 20;
+    private String nomDepartement;
+    private int nbrEmploye;
+    private Employe[] tabEmploye = new Employe[getMAX()];
+
+    public int getMAX() {
+        return MAX;
+    }
+
+    public String getNomDepartement() {
+        return nomDepartement;
+    }
+
+    public void setNomDepartement(String nomDepartement) {
+        this.nomDepartement = nomDepartement;
+    }
+
+    public int getNbrEmploye() {
+        return nbrEmploye;
+    }
+
+    public void setNbrEmploye(int nbrEmploye) {
+        this.nbrEmploye = nbrEmploye;
+    }
+
+    public Employe getTabEmploye(int i) {
+        return tabEmploye[i];
+    }
+
+    public void setTabEmploye(int i, Employe employe) {
+        this.tabEmploye[i] = employe;
+    }
+
+    //Constructeurs	
+    public Departement() {
+        this.setNomDepartement("Departement par defaut");
+    }
+
+    //Constructeur demandant le nom du departement
+    public Departement(String nomDepartement) {
+        this.setNomDepartement(nomDepartement);
+    }
+    //toString
+
+    @Override
+    public String toString() {
+        try {
+            return getNomDepartement() + ": " + getNbrEmploye() + " Employes \n Masse Salariale: " + masseSalariale() + "\n Moyenne Salariale: " + moyenneSalariale();
+        } catch (Exception e) {
+            throw new ArithmeticException(e.toString());
+        }
+    }
+
+    //Autres Methodes
+    //Calcul de la masse salariale
+    public double masseSalariale() {
+        double masse = 0;
+        for (int i = 0; i < getNbrEmploye(); i++) {
+            masse += getTabEmploye(i).salaireBrut();
+        }
+
+        return masse;
+    }//Calcul de la moyenne salariale 
+
+    public double moyenneSalariale() throws Exception {
+        try {
+            return masseSalariale() / getNbrEmploye();
+        } catch (Exception e) {
+            throw new ArithmeticException("Il n'y a pas d'employés!!");
+        }
+    }
+
+    //Methode permettant d'ajouter un employe au bout de tabEmploye
+    public void addEmploye(String nom, String prenom, String date, double salaireEmploye, int heures) throws Exception {
+        setTabEmploye(getNbrEmploye(), new Employe(nom, prenom, date, heures, salaireEmploye));
+        setNbrEmploye(getNbrEmploye() + 1);
+    }
+    //Main pour tester la classe
 	/*public static void main(String[] args) {
-		Departement dep1= new Departement("Programmation");
-		dep1.addEmploye(20);
-		System.out.println(dep1);
-		System.out.println("Masse salarial:"+dep1.moyenneSalariale());
-		System.out.println("Moyenne salarial:"+dep1.moyenneSalariale());
-	}*/
+     Departement dep1= new Departement("Programmation");
+     dep1.addEmploye(20);
+     System.out.println(dep1);
+     System.out.println("Masse salarial:"+dep1.moyenneSalariale());
+     System.out.println("Moyenne salarial:"+dep1.moyenneSalariale());
+     }*/
 
 }
--- Vendeur.java
+++ Vendeur.java
@@ -1,72 +1,106 @@
 /**
  *
- * @author usager
+ * @author Patrick Domingues 09/04/2016
+ * @author2 Raphael Duchaine 12/04/2016
  */
-public class Vendeur extends Employe implements Finance{
+public class Vendeur extends Employe implements Finance {
+
     //2 attributs supplémentaires
-    private int montantVentes ;
-    private double tauxCommission ;
-    
+    private int montantVentes;
+    private double tauxCommission;
+
     //Get-Set
     public int getMontantVentes() {
         return montantVentes;
     }
-    public void setMontantVentes(int unMontantVentes) {
-        this.montantVentes = unMontantVentes;
+
+    public void setMontantVentes(int montantVentes) throws Exception {
+        if (0 <= montantVentes || montantVentes <= 10000) {
+            this.montantVentes = montantVentes;
+        } else {
+            throw new Exception("Montant de ventes invalide (n'est pas entre 0 et 10000)");
+        }
     }
+
     public double getTauxCommission() {
         return tauxCommission;
     }
-    public void setTauxCommission(double unTauxCommission) {
-        this.tauxCommission = unTauxCommission;
+
+    public void setTauxCommission(double tauxCommission) throws Exception {
+        if (0 <= tauxCommission || tauxCommission <= 100) {
+            this.tauxCommission = tauxCommission;
+        } else {
+            throw new Exception("Taux de commission invalide (n'est pas entre 0 et 100)");
+        }
     }
-    
+
     //constructeur sans paramètres
-    public Vendeur(){
+    public Vendeur() throws Exception {
         super();
         setMontantVentes(0);
         setTauxCommission(0);
-        }
-    
+    }
+
     //constructeur avec paramètres
-    public Vendeur(String unNom, String unPrenom, String uneDate, int desHeures, double unTauxHoraire, int unMontantVentes, double unTauxCommission){
+    public Vendeur(String unNom, String unPrenom, String uneDate, int desHeures, double unTauxHoraire, int unMontantVentes, double unTauxCommission) throws Exception {
         super(unNom, unPrenom, uneDate, desHeures, unTauxHoraire);
         setMontantVentes(unMontantVentes);
         setTauxCommission(unTauxCommission);
     }
+    
+    //accès aux informations
+    @Override
+    public String afficher() {
+        return "\t\tVENDEUR"
+                + "\nCode d'accès: "+ codeAcces()
+                + "\nNom: " + getNom()
+                + "\nPrénom: " + getPrenom()
+                + "\nDate de naissance(JJ/MM/AAAA): " + getDate()
+                + "\nMot de passe: " + getMotDePasse()
+                + "\nHeures travaillées: " + getHeures()
+                + "\nTaux horaire: "+ getTauxHoraire() + "$/heures"
+                + "\nSalaire brut: "+ salaireBrut();
+    }
+
+    @Override
+    public String toString() {
+        return "VENDEUR: "+codeAcces() + ", " + getNom() + ", " + getPrenom() + ", " + salaireBrut();
+    }
+
     //Surdéfinition de la méthode initMotPasse()
     @Override
-    public void initMotPasse(){
+    public void initMotPasse() {
         super.setMotDePasse("vendeur");
     }
-    
+
     //Surdéfinition de la méthode salaireBrut(){
     @Override
-    public double salaireBrut(){
+    public double salaireBrut() {
         return super.salaireBrut() + getMontantVentes() * getTauxCommission();
     }
-    
+
     //Surdéfinition de la méthode augmenter() de l'interface Finance
     @Override
-    public double augmenter(){
+    public double augmenter() {
         return getTauxCommission() + TAUX_AUGMENTATION;
     }
-    
+
     //Surdéfinition de la méthode reduire() de l'interface Finance
     @Override
-    public double reduire(){
+    public double reduire() {
         return getTauxCommission() - TAUX_DEDUCTION;
-    }    
-          
-}
+    }
 
+}
 
 //Interface Finance contenant des variables finales et des méthodes abstraites
 interface Finance {
+
     final double TAUX_DEDUCTION = 0.15;
     final double TAUX_AUGMENTATION = 0.10;
-    
-    public double augmenter();   
+
+    public double augmenter();
+
     public double reduire();
-    
+
 }
